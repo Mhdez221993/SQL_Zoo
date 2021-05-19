10f08c62a6b5de93ca757b3fe6cc029c27ede967
Moises <mhdezcoronado@gmail.com>
2021-05-19 16:40:48 +0200

    Query countries & population from South America

--- select_from_world.sql
+++ select_from_world.sql
@@ -5,3 +5,9 @@ SELECT name, continent, population FROM world
 SELECT name
   FROM world
  WHERE population > 200000000
+
+SELECT name, gdp/population FROM world
+  where population > 200000000;
+
+SELECT name, population/1000000 FROM world
+  WHERE continent = 'South America'
