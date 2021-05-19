90c1081445382432369a69d75373053994be5be0
Moises <mhdezcoronado@gmail.com>
2021-05-19 23:20:26 +0200

    Query using cases

--- select_from_nobel.sql
+++ select_from_nobel.sql
@@ -38,3 +38,14 @@ SELECT * FROM nobel WHERE winner = 'PETER GRÜNBERG'
 SELECT * FROM nobel WHERE winner = 'EUGENE O''NEI'
 
 SELECT winner, yr, subject FROM nobel WHERE winner LIKE 'Sir%' ORDER BY yr DESC, winner
+
+
+SELECT winner, subject
+  FROM nobel
+  WHERE yr=1984
+  ORDER BY CASE WHEN subject IN ('Chemistry', 'Physics') THEN 1 ELSE 0 END, subject, winner
+
+SELECT winner, subject
+  FROM nobel
+  WHERE yr=1984
+  ORDER BY CASE WHEN subject IN ('Physics', 'Chemistry') THEN 1 ELSE 0 END, subject, winner;
