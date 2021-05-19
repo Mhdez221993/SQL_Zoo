0f6981226edc84beb99a96d8d4466323fdfd17a9
Moises <mhdezcoronado@gmail.com>
2021-05-19 17:23:22 +0200

    Query countries, population, gdp & ROUND to 2 decimal places

--- select_from_world.sql
+++ select_from_world.sql
@@ -24,3 +24,6 @@ SELECT name, population, area FROM world
 SELECT name, population, area FROM world
   WHERE area > 3000000 AND population < 250000000 OR 
   area < 3000000 AND population > 250000000
+
+SELECT name, ROUND(population/1000000,2), ROUND(gdp/1000000000, 2) FROM world
+  WHERE continent = 'South America'
