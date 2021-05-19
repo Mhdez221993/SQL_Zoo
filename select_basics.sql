ffddd8ccde50d3ac447347079a53db94b8a13c90
Moises <mhdezcoronado@gmail.com>
2021-05-19 15:37:10 +0200

    Query name and population BETWEEN 1000000 AND 1250000

--- select_basics.sql
+++ select_basics.sql
@@ -1,3 +1,5 @@
+-- SELECT basics
+
 -- show the population of Germany
 SELECT population FROM world
   WHERE name = 'Germany'
@@ -9,3 +11,15 @@ SELECT name, population FROM world
 -- shows countries with an area of 200,000-350,000 sq. km.
 SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000
+
+
+
+-- SELECT Quiz
+SELECT name, population
+  FROM world
+ WHERE population BETWEEN 1000000 AND 1250000
+
+
+SELECT name, population
+      FROM world
+      WHERE name LIKE 'Al%'
