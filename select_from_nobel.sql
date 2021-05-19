a6c6d3fc6421d93c399c765413a3ffa8fb53e631
Moises <mhdezcoronado@gmail.com>
2021-05-19 22:41:31 +0200

    Query all ditails for SELECT PETER GRÜNBERG

--- select_from_nobel.sql
+++ select_from_nobel.sql
@@ -32,3 +32,5 @@ SELECT yr, subject, winner FROM nobel
 
 SELECT yr, subject, winner FROM nobel
   WHERE subject = 'Medicine' AND yr < 1910 OR subject = 'Literature' AND yr >= 2004
+
+SELECT * FROM nobel WHERE winner = 'PETER GRÜNBERG'
