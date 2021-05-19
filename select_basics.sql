8692482d3dbf2f2a7873eedd121998baad64f77f
Moises <mhdezcoronado@gmail.com>
2021-05-19 15:04:08 +0200

    Query name and  area using BETWEEN AND

--- select_basics.sql
+++ select_basics.sql
@@ -1,5 +1,11 @@
+-- show the population of Germany
 SELECT population FROM world
   WHERE name = 'Germany'
 
+-- Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
 SELECT name, population FROM world
   WHERE name IN ('Sweden', 'Norway', 'Denmark');
+
+-- shows countries with an area of 200,000-350,000 sq. km.
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
