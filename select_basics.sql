284631260fb7a9e1984a5110f4284f4adb7c4b9d
Moises <mhdezcoronado@gmail.com>
2021-05-19 15:38:29 +0200

    Query name starting with Al

--- select_basics.sql
+++ select_basics.sql
@@ -19,7 +19,6 @@ SELECT name, population
   FROM world
  WHERE population BETWEEN 1000000 AND 1250000
 
-
 SELECT name, population
-      FROM world
-      WHERE name LIKE 'Al%'
+  FROM world
+  WHERE name LIKE 'Al%'
