36f3ab7584f4907104d0a29a613dd007f9c7559b
Moises <mhdezcoronado@gmail.com>
2021-05-20 22:26:29 +0200

    Query continents with population > 100m

--- sum_and_count.sql
+++ sum_and_count.sql
@@ -15,3 +15,7 @@ group by continent
 select continent, count(name) from world
 where population > 10000000
 group by continent
+
+select continent from world x
+where 100000000 < ALL(select sum(population) from world y where x.continent = y.continent)
+group by continent
