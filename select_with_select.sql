4230eccd7ae1378033be2afd8e127e5e9190b492
Moises <mhdezcoronado@gmail.com>
2021-05-20 10:04:59 +0200

    Query country where gdp > UK

--- select_with_select.sql
+++ select_with_select.sql
@@ -4,3 +4,7 @@ SELECT name FROM world
   WHERE population >
      (SELECT population FROM world
       WHERE name='Russia')
+
+SELECT name FROM WORLD
+  WHERE continent = 'Europe' AND 
+  gdp/population > (SELECT gdp/population FROM world WHERE name = 'United Kingdom')
