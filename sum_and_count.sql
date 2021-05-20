8346bb487c2b14a6232918de0ba7290eeeae52d8
Moises <mhdezcoronado@gmail.com>
2021-05-20 21:23:54 +0200

    Query population by continent

--- sum_and_count.sql
+++ sum_and_count.sql
@@ -8,3 +8,6 @@ select sum(gdp) from world where continent = 'Africa'
 select count(name) from world where area > 1000000
 
 select sum(population) from world where name IN ('Estonia', 'Latvia', 'Lithuania')
+
+select continent, count(name) from world
+group by continent
