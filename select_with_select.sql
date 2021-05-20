d8d5f9eb42caf39f87b115b84c8cd544eb77fd5b
Moises <mhdezcoronado@gmail.com>
2021-05-20 14:33:39 +0200

    Query gdp > europe gdp

--- select_with_select.sql
+++ select_with_select.sql
@@ -15,3 +15,13 @@ SELECT name, continent FROM world
 SELECT name, population FROM world
   WHERE population > (SELECT population FROM world WHERE name = 'Canada') AND
   population < (SELECT population FROM world WHERE name = 'Poland')
+
+SELECT name, 
+  CONCAT(ROUND(100 * population/(SELECT population FROM world WHERE name = 'Germany')),'%')
+  FROM world
+  WHERE continent = 'Europe'
+
+SELECT name FROM world
+  WHERE gdp > ALL
+   (SELECT gdp FROM world
+   WHERE continent = 'Europe' AND gdp IS NOT NULL)
