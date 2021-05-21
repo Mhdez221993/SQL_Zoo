d694dc47f3213896c63936ee449100c2f63969c2
Moises <mhdezcoronado@gmail.com>
2021-05-21 23:06:51 +0200

    Query teacher with null dept inclunding null

--- using_null.sql
+++ using_null.sql
@@ -3,3 +3,7 @@ select name from teacher where dept is null
 SELECT teacher.name, dept.name
   FROM teacher INNER JOIN dept
            ON (teacher.dept=dept.id)
+
+SELECT teacher.name, dept.name
+  FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
