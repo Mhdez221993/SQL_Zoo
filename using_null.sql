9eab715d93094976f435f9da796c0a84ee94f689
Moises <mhdezcoronado@gmail.com>
2021-05-21 23:09:46 +0200

    Query teacher with null dept

--- using_null.sql
+++ using_null.sql
@@ -7,3 +7,7 @@ SELECT teacher.name, dept.name
 SELECT teacher.name, dept.name
   FROM teacher LEFT JOIN dept
            ON (teacher.dept=dept.id)
+
+SELECT teacher.name, dept.name
+  FROM teacher RIGHT JOIN dept
+           ON (teacher.dept=dept.id)
