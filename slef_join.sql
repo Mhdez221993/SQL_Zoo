65ab3b155b527cf00172970ecd6e05748e02a8bd
Moises <mhdezcoronado@gmail.com>
2021-05-22 13:25:05 +0200

    use of self join

--- slef_join.sql
+++ slef_join.sql
@@ -17,3 +17,8 @@ SELECT company, num, COUNT(*)
   GROUP BY company, num
   having count(*) = 2
 
+-- 5.Execute the self join shown and observe that b.stop gives all the places you can get to from Craiglockhart, without changing routes. Change the query so that it shows the services from Craiglockhart to London Road.
+SELECT a.company, a.num, a.stop, b.stop
+  FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  WHERE a.stop=53 and b.stop = 149
