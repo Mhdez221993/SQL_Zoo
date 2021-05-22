0466d3f5e64821014bce698bc61cc4394497390b
Moises <mhdezcoronado@gmail.com>
2021-05-22 13:30:22 +0200

    use of self join on two tables

--- slef_join.sql
+++ slef_join.sql
@@ -22,3 +22,11 @@ SELECT a.company, a.num, a.stop, b.stop
   FROM route a JOIN route b ON
   (a.company=b.company AND a.num=b.num)
   WHERE a.stop=53 and b.stop = 149
+
+-- 6.The query shown is similar to the previous one, however by joining two copies of the stops table we can refer to stops by name rather than by number. Change the query so that the services between 'Craiglockhart' and 'London Road' are shown. If you are tired of these places try 'Fairmilehead' against 'Tollcross'
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' and stopb.name = 'London Road'
