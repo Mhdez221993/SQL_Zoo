9d93e09149123e87e1d647941dd0d5512577dc22
Moises <mhdezcoronado@gmail.com>
2021-05-22 11:44:03 +0200

    Query id & name for stops on 4 LRT service

--- slef_join.sql
+++ slef_join.sql
@@ -3,3 +3,7 @@
 select count(name) from stops
 
 select id from stops where name = 'Craiglockhart'
+
+select id, name from stops join route on id = stop
+  where num = 4 AND company = 'LRT'
+  order by pos
