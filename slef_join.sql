5673e75e7f4cda8a49ac8cf8a6b69aa3281d3ef3
Moises <mhdezcoronado@gmail.com>
2021-05-22 13:05:39 +0200

    Query num of rout where stop = 149 or 53

--- slef_join.sql
+++ slef_join.sql
@@ -7,3 +7,8 @@ select id from stops where name = 'Craiglockhart'
 select id, name from stops join route on id = stop
   where num = 4 AND company = 'LRT'
   order by pos
+
+SELECT company, num, COUNT(*)
+  FROM route WHERE stop=149 OR stop=53
+  GROUP BY company, num
+  having count(*) = 2
